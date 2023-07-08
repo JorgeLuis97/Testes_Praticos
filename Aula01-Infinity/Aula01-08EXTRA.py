e2c355b84ff3477782577d11b023b979ec9bf971
Jorge Luis Gomes Rabelo <Jorgeluisgomesrabelo@gmail.com>
2023-07-08 11:34:45 -0300

    V2
    Terminei o codigo de impar ou par

--- Aula01-Infinity/Aula01-08EXTRA.py
+++ Aula01-Infinity/Aula01-08EXTRA.py
@@ -10,4 +10,16 @@ for i in range(1, 3):
     listateste.append(valor)
     lista.append(listateste)
 
+segundos_elementos = [sublista[2] for sublista in lista]
+soma = sum(segundos_elementos)
+
 print(lista)
+
+if soma % 2 == 0:
+    vencedor = lista[0][0] if lista[0][1] == 'PAR' else lista[1][0]
+else:
+    vencedor = lista[1][0] if lista[0][1] == 'PAR' else lista[0][0]
+
+print(f"O vencedor é: {vencedor}")
+
+
